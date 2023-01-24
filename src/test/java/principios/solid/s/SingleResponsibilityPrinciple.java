fe567e3b4f49cd760049bb6c9dd19ebaffa438ce
EdwinDMendezR <61098711+EdwinDMendezR@users.noreply.github.com>
2023-01-24 15:37:21 -0500

    POO: Single Responsibility Principle

---
+++ src/test/java/principios/solid/s/SingleResponsibilityPrinciple.java
@@ -0,0 +1,24 @@
+package principios.solid.s;
+
+public class SingleResponsibilityPrinciple {
+
+    private String name;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+}
